dff95296286086ab77aae6eab35ed4ae29fe5346
unknown <FLAVIO_5364@UIRAPURU.BR>
2026-08-19 20:17:48 -0300

    pamIII

---
+++ pamIII/LogincomShared1/app/src/main/java/com/flavio/logincomshared/MainActivity.java
@@ -0,0 +1,91 @@
+package com.flavio.logincomshared;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText nome, email, senha;
+    Button entrar, novo;
+    CheckBox lembrar;
+    SharedPreferences preferences;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        initComponents();
+
+        entrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(validarDados() ){
+                    if (lembrar.isChecked()){
+                        preferences = getSharedPreferences("login", 0 );
+                        SharedPreferences.Editor dados = preferences.edit();
+                    dados.putString("Nome", nome.getText().toString());
+                    dados.putString("Email", email.getText().toString());
+                    dados.putString("Senha", senha.getText().toString());
+
+                    }
+                }
+                nome.setText("");
+                email.setText("");
+                senha.setText("");
+                //ao clicar no botão entrar é direcionado a home page
+                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    private boolean validarDados() {
+        boolean retorno = true;
+
+        if(nome.getText().toString().isEmpty()){
+            retorno = false;
+            nome.setError("Campo nome não pode ficar vazio");
+        }
+        if (email.getText().toString().isEmpty()){
+            retorno = false;
+            email.setError("Campo email não pode ficar vazio");
+        }
+        if (senha.getText().toString().isEmpty()){
+                retorno = false;
+                senha.setError("campo senha não pode ficar fazio");
+            }
+
+
+        return  retorno;
+
+    }
+
+    private void initComponents() {
+        nome = findViewById(R.id.edt_nome);
+        email = findViewById(R.id.edt_email);
+        senha = findViewById(R.id.edt_senha);
+        entrar = findViewById(R.id.btn_entrar);
+        novo = findViewById(R.id.btn_novo);
+        lembrar = findViewById(R.id.ckb_lembrar);
+    }
+}
